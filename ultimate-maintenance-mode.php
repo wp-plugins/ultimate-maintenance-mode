3d6a9c607beb0980658dc24b475aee0c1763ad1d
johnnytee <johnnytee@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-02-14 15:42:22 +0000

    Version 1.0.0 release
    git-svn-id: https://plugins.svn.wordpress.org/ultimate-maintenance-mode/trunk@504854 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ ultimate-maintenance-mode.php
@@ -0,0 +1,31 @@
+<?php
+/*
+Plugin Name: Ultimate Maintenance Mode
+Plugin URI: http://seedprod.com
+Description: Displays a screenshot of website with an overlayed window with the reason your site is down. 
+Version: 0.1.0
+Author: SeedProd
+Author URI: http://seedprod.com
+License: GPLv2
+Copyright 2011  John Turner (email : john@seedprod.com, twitter : @johnturner)
+*/
+
+/**
+ * Init
+ *
+ * @package WordPress
+ * @subpackage Ultimate_Maintenence_Mode
+ * @since 0.1
+ */
+
+/**
+ * Require config to get our initial values
+ */
+define( 'UMM_PLUGIN_DIR', WP_PLUGIN_DIR . '/' . dirname( plugin_basename( __FILE__ ) ) );
+define( 'UMM_PLUGIN_URL', WP_PLUGIN_URL . '/' . dirname( plugin_basename( __FILE__ ) ) );
+
+load_plugin_textdomain('ultimate-maintenance-mode',false, dirname( plugin_basename( __FILE__ ) ) . '/languages/');
+
+require_once('framework/framework.php');
+require_once('inc/config.php');
+
